4a675dd1b908ccec843dac1c64896e9111f9611d
prolapse <117679376+PR0LAPSE@users.noreply.github.com>
2023-03-17 12:45:04 +0500

    Update colab_models_list.py

--- scripts/colab_models_list.py
+++ scripts/colab_models_list.py
@@ -5,7 +5,7 @@ from modules import scripts, script_callbacks
 
 def on_ui_tabs():     
     with gr.Blocks() as colab_models_list:
-        html_url = "https://raw.githubusercontent.com/PR0LAPSE/colab_models_list/main/scripts/models.html"
+        html_url = "https://raw.githubusercontent.com/PR0LAPSE/PR0LAPSE.github.io/main/models.html"
         html_text = requests.get(html_url).text
         gr.HTML(f"{html_text}")
     return (colab_models_list, "описание моделей", "colab_models_list"),
